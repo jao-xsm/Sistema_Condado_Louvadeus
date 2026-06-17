0f5d98e3593b7a3be7653d3001a6e3584e5eb262
joão victor soares monteiro <jaovicxsm@icloud.com>
2026-06-17 02:11:17 -0300

    msm do anterior

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from src.routes.usuario_route import router as usuario_router
+from src.routes.chale_route import router as chale_router
 
 app = FastAPI(
     title="API - Condado Louvadeus",
@@ -8,6 +9,7 @@ app = FastAPI(
 )
 
 app.include_router(usuario_router)
+app.include_router(chale_router)
 
 #para testar se esta no ar
 @app.get("/", tags=["Status"])
