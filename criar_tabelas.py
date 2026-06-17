16ef8b1bf6229eaac77f969810bc8c0762617e9d
joão victor soares monteiro <jaovicxsm@icloud.com>
2026-06-17 02:11:00 -0300

    add tabela dos chales ao neon

--- criar_tabelas.py
+++ criar_tabelas.py
@@ -1,6 +1,7 @@
 from src.database import engine, Base
 
 from src.models.usuario import Usuario
+from src.models.chale import Chale
 
 print("A conectar à Neon e a gerar as tabelas o PostgreSQL...")
 
