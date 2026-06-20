439bd6c8315e650ff3794e009b193449cba79f9a
Manuela Grimaldi Hansel <manela.grimaldi@gmail.com>
2026-06-20 19:15:37 -0300

    add modulo de CORS para aceitar requisições do frontend

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+# importando o modulo de CORS
 
 from src.routes.usuario_route import router as usuario_router
 from src.routes.chale_route import router as chale_router
@@ -6,6 +8,13 @@ from src.routes.chale_route import router as chale_router
 app = FastAPI(
     title="API - Condado Louvadeus",
     description="Sistema de gerenciamento de hospedagens da pousada."
+)
+
+app.add_middleware( # configura o servidor para aceitar requisições do frontend
+    CORSMiddleware, 
+    allow_origins=["*"], # aceita requisições de qualquer endereço (depois mudar p o endereço só do site msm)
+    allow_methods=["*"], # aceita qualquer método (GET, POST, etc.)
+    allow_headers=["*"], # aceita qualquer cabeçalho
 )
 
 app.include_router(usuario_router)
