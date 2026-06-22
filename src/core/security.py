84c61868df30827599c3c15d8d7cbc7338757b35
joão victor soares monteiro <jaovicxsm@icloud.com>
2026-06-22 02:02:07 -0300

    add metodos de retorno do tipo de usuario para a api

--- src/core/security.py
+++ src/core/security.py
@@ -1,10 +1,14 @@
 import jwt
 from datetime import datetime, timedelta, timezone
 import bcrypt
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 
 SECRET_KEY = "chave_super_secreta_cl"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRES_MINUTES = 60
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/usuarios/login-swagger")
 
 def verificar_senha(senha_digitada: str, senha_criptografada_bd: str) -> bool:
     return bcrypt.checkpw(
@@ -21,3 +25,38 @@ def criar_token_acesso(dados_usuario: dict) -> str:
     token_jwt = jwt.encode(dados_a_criptografar, SECRET_KEY, algorithm=ALGORITHM)
 
     return token_jwt
+
+def obter_usuario_atual(token: str = Depends(oauth2_scheme)) -> dict:
+    erro_autenticacao = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Token de acesso inválido ou expirado.",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except jwt.PyJWTError:
+        raise erro_autenticacao
+    
+def obter_hospede_atual(payload: dict = Depends(obter_usuario_atual)) -> dict:
+    if payload.get("tipo_usuario") != "hospede":
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Acesso negado. Esta operação é exclusiva para perfis de hóspedes."
+        )
+    
+    if "id" not in payload and "sub" in payload:
+        payload["id"] = payload["sub"]
+
+    return payload
+
+def obter_anfitriao_atual(payload: dict = Depends(obter_usuario_atual)) -> dict:
+    if payload.get("tipo_usuario") != "anfitriao":
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Acesso negado. Esta operação é esclusiva para o Anfitrião."
+        )
+    if "id" not in payload and "sub" in payload:
+        payload["id"] = payload["sub"]
+
+    return payload
