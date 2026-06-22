3f5a3003be5c6af2631535ecc53910f9a4378b17
joão victor soares monteiro <jaovicxsm@icloud.com>
2026-06-22 02:02:39 -0300

    create classe da reserva

---
+++ src/models/reserva.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, Float, String, Date, ForeignKey
+from sqlalchemy.orm import relationship
+from src.database import Base
+
+class Reserva(Base):
+    __tablename__ = "reservas"
+    id = Column(Integer, primary_key=True, index=True)
+
+    hospede_id = Column(Integer, ForeignKey("usuarios.id", ondelete="CASCADE"), nullable=False)
+    chale_id = Column(Integer, ForeignKey("chales.id", ondelete="CASCADE"), nullable=False)
+
+    data_checkin = Column(Date, nullable=False)
+    data_checkout = Column(Date, nullable=False)
+
+    valor_total = Column(Float, nullable=False)
+
+    status = Column(String, default="PENDENTE", nullable=False)
+
+    hospede = relationship("Usuario")
+    chale = relationship("Chale")
